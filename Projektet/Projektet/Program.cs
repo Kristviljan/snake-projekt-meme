7d6c165eda8c5f60098b40c5811a5f0cab02e608
Maffia9770 <maffia9770@gmail.com>
2017-03-13 13:39:23 +0100

    engine

--- Projektet/Projektet/Program.cs
+++ Projektet/Projektet/Program.cs
@@ -14,6 +14,7 @@ namespace Projektet
         [STAThread]
         static void Main()
         {
+            Engine engine = new Engine;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
