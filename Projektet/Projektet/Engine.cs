a1ae3c459106590375d8db16fa4190b88a5dcfde
Johan Liljeqvist <johan_liljeqvist@hotmail.com>
2017-03-13 14:24:47 +0100

    engine grej

--- Projektet/Projektet/Engine.cs
+++ Projektet/Projektet/Engine.cs
@@ -7,23 +7,33 @@ namespace Projektet
 {
     public class Engine
     {
+        private Timer timer;
+        private Form1 form;
+
+
         public Engine()
         {
-            Timer timer;
-            Form1 form;
+            timer = new Timer();
+            form = new Form1();
+
+
         }
+
         public void Run()
         {
 
         }
+
         public void AddFood()
         {
 
         }
+
         private void TimerEventHandler(Object obj, EventArgs args)
         {
 
         }
+
         private void Draw(Object obj, PaintEventArgs args)
         {
 
