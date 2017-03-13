3831648754480fd6df8126d6280d597bae7e06e8
Maffia9770 <maffia9770@gmail.com>
2017-03-13 16:27:57 +0100

    DAFSGBC

--- Projektet/Projektet/Rectangle.cs
+++ Projektet/Projektet/Rectangle.cs
@@ -9,15 +9,15 @@ namespace Projektet
 {
     public class Rectangle
     {
-        private Pen pen = new Pen(Color.Black);
-        private Point point = new Point();
+        public Pen pen = new Pen(Color.Black);
+        public Point point = new Point();
 
         public Rectangle(int x, int y)
         {
             this.point.X = x;
             this.point.Y = y;
         }
-
+       // public abstract void Draw(Graphics g);
         public void Draw(Graphics g)
         {
             g.DrawRectangle(pen, Point.X, Point.Y, 50, 50);
