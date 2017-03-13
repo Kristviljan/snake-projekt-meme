38e64fa510fc7df3caf41c244ba23fa31ff001fa
Maffia9770 <maffia9770@gmail.com>
2017-03-13 19:09:09 +0100

    s

--- Projektet/Projektet/Form1.Designer.cs
+++ Projektet/Projektet/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 600);
